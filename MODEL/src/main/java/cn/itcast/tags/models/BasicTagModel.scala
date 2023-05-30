08abf96a124c9195c62fa3890da855b1b197d274
sunk <sunkankanisme@163.com>
2023-05-30 15:04:13 +0800

    增加 config.properties 重构模板方法类

---
+++ MODEL/src/main/java/cn/itcast/tags/models/BasicTagModel.scala
@@ -0,0 +1,141 @@
+package cn.itcast.tags.models
+
+import cn.itcast.tags.config.ModelConfig
+import cn.itcast.tags.meta.HBaseMeta
+import cn.itcast.tags.tools.HBaseTools
+import org.apache.hadoop.hbase.client.{Put, Result}
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.spark.SparkConf
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.storage.StorageLevel
+
+trait BasicTagModel extends Logging {
+
+    // 变量声明
+    var spark: SparkSession = _
+
+    // 1. 初始化：构建SparkSession实例对象
+    def init(): Unit = {
+        System.setProperty("HADOOP_USER_NAME", "root")
+
+        spark = {
+            val sparkConf: SparkConf = new SparkConf()
+                // 是否使用本地模式
+                .setMaster("local[*]")
+                .setAppName(this.getClass.getSimpleName.stripSuffix("$"))
+                .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+                .registerKryoClasses(Array(classOf[ImmutableBytesWritable], classOf[Result], classOf[Put]))
+                .set("spark.sql.shuffle.partitions", "4")
+
+            SparkSession.builder()
+                .config(sparkConf)
+                // 是否启用与Hive集成
+                .enableHiveSupport()
+                // 设置与Hive集成: 读取Hive元数据MetaStore服务
+                .config("hive.metastore.uris", ModelConfig.HIVE_METASTORE_URIS)
+                // 设置数据仓库目录，将 SparkSQL 数据仓库目录与 Hive 的位置一致
+                .config("spark.sql.warehouse.dir", ModelConfig.SPARK_SQL_WAREHOUSE_DIR)
+                .getOrCreate()
+        }
+    }
+
+    // 2. 准备标签数据：依据标签ID从MySQL数据库表tbl_basic_tag获取标签数据
+    def getTagData(tagId: Long): DataFrame = {
+        spark.read.format("jdbc")
+            .option("driver", ModelConfig.MYSQL_JDBC_DRIVER)
+            .option("url", ModelConfig.MYSQL_JDBC_URL)
+            .option("dbtable", ModelConfig.tagTable(tagId))
+            .option("user", ModelConfig.MYSQL_JDBC_USERNAME)
+            .option("password", ModelConfig.MYSQL_JDBC_PASSWORD)
+            .load()
+    }
+
+    // 3. 业务数据：依据业务标签规则rule，从数据源获取业务数据
+    def getBusinessData(basicTagDF: DataFrame): DataFrame = {
+        import basicTagDF.sparkSession.implicits._
+
+        // 获取和解析规则，转换为 Map 集合
+        val tagRules = basicTagDF.filter($"level" === 4).head().getAs[String]("rule")
+
+        val tagRuleMap = tagRules.split("\\n").map(line => {
+            val Array(k, v) = line.trim.split("=")
+            (k, v)
+        }).toMap
+
+        // 判断数据源读取业务数据
+        var businessDF: DataFrame = null
+        if ("hbase".equals(tagRuleMap("inType").toLowerCase())) {
+            // 封装标签规则中的数据源信息至 HBaseMeta 对象中
+            val hBaseMeta = HBaseMeta.getHBaseMeta(tagRuleMap)
+            // 从 HBase 加载数据
+            businessDF = HBaseTools.read(spark,
+                hBaseMeta.zkHosts,
+                hBaseMeta.zkPort,
+                hBaseMeta.hbaseTable,
+                hBaseMeta.family,
+                hBaseMeta.selectFieldNames.split(","))
+        } else {
+            System.exit(0)
+        }
+
+        // businessDF.printSchema()
+        // businessDF.show(10, truncate = false)
+
+        businessDF
+    }
+
+    // 4. 构建标签：依据业务数据和属性标签数据建立标签
+    def doTag(businessDF: DataFrame, tagDF: DataFrame): DataFrame
+
+    // 5. 5. 保存画像标签数据至HBase表
+    def saveTag(modelDF: DataFrame): Unit = {
+        // 存储结果到 HBase
+        if (modelDF != null) {
+            HBaseTools.write(modelDF,
+                ModelConfig.PROFILE_TABLE_ZK_HOSTS,
+                ModelConfig.PROFILE_TABLE_ZK_PORT,
+                ModelConfig.PROFILE_TABLE_NAME,
+                ModelConfig.PROFILE_TABLE_FAMILY_DETAIL,
+                ModelConfig.PROFILE_TABLE_ROWKEY_COL)
+        }
+    }
+
+    // 6. 关闭资源：应用结束，关闭会话实例对象
+    def close(): Unit = {
+        if (spark != null) {
+            spark.stop()
+        }
+    }
+
+    // 规定标签模型执行流程顺序
+    def executeModel(tagId: Long): Unit = {
+        // a. 初始化
+        init()
+
+        try {
+            // b. 获取标签数据
+            val tagDF: DataFrame = getTagData(tagId)
+
+            // basicTagDF.show()
+            tagDF.persist(StorageLevel.MEMORY_AND_DISK)
+            tagDF.count()
+
+            // c. 获取业务数据
+            val businessDF: DataFrame = getBusinessData(tagDF)
+            //businessDF.show()
+
+            // d. 计算标签
+            val modelDF: DataFrame = doTag(businessDF, tagDF)
+            //modelDF.show()
+
+            // e. 保存标签
+            saveTag(modelDF)
+            tagDF.unpersist()
+        } catch {
+            case e: Exception => e.printStackTrace()
+        } finally {
+            close()
+        }
+    }
+}
